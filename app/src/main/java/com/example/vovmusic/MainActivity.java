227f405aaf2dec134a7dab0fb7df8611169ea5f4
Phi Vu <hoalong369741@gmail.com>
2021-11-07 20:12:17 +0700

    Lần 4

--- app/src/main/java/com/example/vovmusic/MainActivity.java
+++ app/src/main/java/com/example/vovmusic/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.vovmusic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +17,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.onboarding);
 
         Button btn1 = (Button) findViewById(R.id.button_sign);
+        TextView tex1 =(TextView) findViewById(R.id.log_in);
+
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setContentView(R.layout.login);
+            }
+        });
+
+        tex1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.register);
             }
         });
 
