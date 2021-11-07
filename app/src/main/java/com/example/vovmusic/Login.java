ec0147e3dcd486779ddc5a9ba42d8181510ca1bb
Phi Vu <hoalong369741@gmail.com>
2021-11-07 20:54:11 +0700

    Lần 5

---
+++ app/src/main/java/com/example/vovmusic/Login.java
@@ -0,0 +1,49 @@
+package com.example.vovmusic;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        TextView signup = (TextView) findViewById(R.id.text_signup);
+        ImageView back = (ImageView)  findViewById(R.id.back);
+        Button signin = (Button) findViewById(R.id.button_sign);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/vovmusic/Register.java
+++ app/src/main/java/com/example/vovmusic/Register.java
@@ -1,36 +1,35 @@
 package com.example.vovmusic;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    @Override
+public class Register extends AppCompatActivity {
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.onboarding);
+        setContentView(R.layout.register);
 
         Button btn1 = (Button) findViewById(R.id.button_sign);
-        TextView tex1 =(TextView) findViewById(R.id.log_in);
+        ImageView back = (ImageView)  findViewById(R.id.back);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Register.class);
+                Intent intent = new Intent(Register.this, Login.class);
                 startActivity(intent);
             }
         });
 
-        tex1.setOnClickListener(new View.OnClickListener() {
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Login.class);
+                Intent intent = new Intent(Register.this, MainActivity.class);
                 startActivity(intent);
             }
         });
