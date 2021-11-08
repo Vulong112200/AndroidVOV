de0ee8d7974fbf12001164cd550311fc84789ec9
Phi Vu <hoalong369741@gmail.com>
2021-11-08 15:54:22 +0700

    Lan 8

--- app/src/main/java/com/example/vovmusic/Reset.java
+++ app/src/main/java/com/example/vovmusic/Reset.java
@@ -2,6 +2,7 @@ package com.example.vovmusic;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,6 +13,25 @@ public class Reset extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.resetpass);
+        ImageView back = (ImageView)  findViewById(R.id.back);
+        Button send = (Button) findViewById(R.id.send);
 
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Reset.this, Login.class);
+                startActivity(intent);
+                Log.d("Login", "onRestart - Màn hình chính được khởi tạo lại.");
+            }
+        });
+
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Reset.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
